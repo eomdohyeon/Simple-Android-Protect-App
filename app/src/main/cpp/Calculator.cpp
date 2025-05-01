bdb53e5722a8d45f285382e841b1e50a54a34d6e
DoHyeon Eom <83337136+eomdohyeon@users.noreply.github.com>
2025-05-01 18:44:45 +0900

    Update Calculator.cpp

--- app/src/main/cpp/Calculator.cpp
+++ app/src/main/cpp/Calculator.cpp
@@ -16,7 +16,7 @@ bool FridaDetect::is_frida_binary() {
     struct dirent *dir_entry;
     char *ptr;
 
-    dir_info = opendir("/data/local/tmp/test");
+    dir_info = opendir("/data/local/tmp");
     if (NULL != dir_info) {
         while (dir_entry = readdir(dir_info)) {
             ptr = strstr(dir_entry->d_name, "frida");
